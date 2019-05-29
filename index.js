496cbbf5ffa5c38b9a3ae21a99f7d44568e8ce46
Vi <liliput_j@yahoo.com>
2019-05-29 11:26:14 -0700

    Fixed typo in index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({'Hello'});
+  res.send('Hello');
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
