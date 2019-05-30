a06cac3c9e0f904508b965abb8c7088b4c4a130c
Vi <liliput_j@yahoo.com>
2019-05-29 20:38:19 -0700

    Added script to package.json

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const { PORT, APP_TOKEN } = keys;
 
 appRoutes(app, APP_TOKEN);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
